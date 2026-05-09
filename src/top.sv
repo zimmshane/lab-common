90873525d20925e37a0c2e9b574e88261dbf4120
zimmshane <zimmshane@gmail.com>
2026-05-08 18:44:43 -0700

    add sprite buffer

--- src/top.sv
+++ src/top.sv
@@ -1,11 +1,12 @@
 `include "clk_divider.sv"
 `include "lcd.sv"
+`include "sprite_buf_EX1.sv"
 
 module top
 (
     input CLK, //FPGA's cock
 
-	output LCD_CLK,//LCD clock. 
+	output LCD_CLK,//LCD clock.
 	output LCD_DEN,
 	output [4:0] LCD_R,
 	output [5:0] LCD_G,
@@ -18,10 +19,22 @@ clk_divider #(.CLK_OUT_FREQ(8_000_000)) clk_div
     .slow_clk(LCD_CLK)
 );
 
+logic [15:0] pixel_data;
+logic [7:0] pixel_addr;
+
+dp_buffer sprite_buf
+(
+    .clk(LCD_CLK),
+    .rdata(pixel_data),
+    .raddr(pixel_addr)
+);
+
 lcd lcd
 (
 	.rst(1'b0),
 	.pclk(LCD_CLK),
+	.pixel(pixel_data),
+	.pixel_address(pixel_addr),
 	.LCD_R(LCD_R),
 	.LCD_B(LCD_B),
 	.LCD_G(LCD_G),
